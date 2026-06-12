722d1b33cfdc120b4e9777fb3399a046ef7597a3
stephane van rossem <stephane.vanrossem@ap.be>
2026-06-12 09:01:14 +0200

    Update docusaurus.config.js

--- security-cursus/docusaurus.config.js
+++ security-cursus/docusaurus.config.js
@@ -20,15 +20,11 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://StephaneVanRossem02.github.io',
+  baseUrl: '/Cursus-Testing-en-Security/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'StephaneVanRossem02',
+  projectName: 'Cursus-Testing-en-Security',
 
   onBrokenLinks: 'throw',
 
